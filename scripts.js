2352d1f912c3f7944e7cc48916f2fe749e1e8bd1
gustavooko <gustavosantos12990@gmail.com>
2024-03-19 08:23:12 -0300

    Formulário

--- scripts.js
+++ scripts.js
@@ -2,6 +2,37 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email =  document.getElementById("email");
+
+var telefone =  document.getElementById("telefone");
+
+var CEP =  document.getElementById("CEP");
+
+var Logradouro =  document.getElementById("Logradouro");
+
+var Número =  document.getElementById("Número");
+
+var Complemento =  document.getElementById("Complemento");
+
+var Bairro =  document.getElementById("Bairro");
+
+var Cidade =  document.getElementById("Cidade");
+
+var Estado =  document.getElementById("Estado");
+
 function alertar(event){
-    alert("Você clicou no botão!!!" + nome.value);
+   //alert("Você clicou no botão!!!" + nome.value);
+
+   saida.innerText = "Nome: " + nome.value +
+    "\n Email: " + email.value + 
+    "\n Telefone:" + telefone.value +
+    "\n CEP:" + CEP.value + 
+    "\n Logradouro:" + Logradouro.value + 
+    "\n Número:" + Número.value + 
+    "\n Complemento:" + Complemento.value + 
+    "\n Bairro:" + Bairro.value + 
+    "\n Cidade:" + Cidade.value +
+    "\n Estado:" + Estado.value;
 }
